83abd5422c14a198e1bc48ccc9ec62d0a68b4e45
Anton <medvedev.workmail@gmail.com>
2017-04-10 15:24:59 +0300

    fix formatting in header.component.ts

--- src/app/commons/header/header.component.ts
+++ src/app/commons/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-	buttonState: boolean;
+  buttonState: boolean;
 
   ngOnInit() {
     this.buttonState = false;
