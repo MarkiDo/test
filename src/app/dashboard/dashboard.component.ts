8a03abbbea1de71e137f1a53b41612776f81ff70
gmarjan1 <marjan.lviv@gmail.com>
2017-04-11 07:27:52 +0000

    added descriptive names

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,20 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  title1: string;
-  title2: string;
-  status1:string;
-  status2: string;
-  data1: string;
-  data2: string;
+  emailScheduler: string;
+  serversConfigurator: string;
+  statusEmailScheduler:string;
+  statusServersConfigurator: string;
+  scheduledEmails: string;
+  serversConfigured: string;
 
   constructor() { 
-    this.title1 = 'Email Scheduler'; 
-    this.title2  = 'Servers Configurator';
-    this.status1 = "On";
-    this.status2 = "Off";
-    this.data1 = "Scheduled emails: 10";
-    this.data2 = "Servers Configured: 5"
+    this.emailScheduler = 'Email Scheduler'; 
+    this.serversConfigurator  = 'Servers Configurator';
+    this.statusEmailScheduler = "On";
+    this.statusServersConfigurator = "Off";
+    this.scheduledEmails = "Scheduled emails: 10";
+    this.serversConfigured = "Servers Configured: 5"
 }
 
   ngOnInit() {
