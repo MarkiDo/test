97cee7de0fab38e18a80c3ac845e076b64b835c3
IrinaMakaruk <irunamakaruk@gmail.com>
2017-04-13 01:21:37 +0300

    fixed comments except unit test

--- src/app/dashboard/widget/widget.component.ts
+++ src/app/dashboard/widget/widget.component.ts
@@ -1,24 +1,28 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {SettingsComponent,ServersConfigurator,EmailScheduler } from 'app/dashboard/widget/settings/settings.component'
-import {MdDialog} from '@angular/material';
+import { SettingsComponent, ServersConfigurator, EmailScheduler } from 'app/dashboard/widget/settings/settings.component'
+import { DashboardComponent } from 'app/dashboard/dashboard.component';
+import { MdDialog } from '@angular/material';
 @Component({
   selector: 'ita-widget',
   templateUrl: './widget.component.html',
   styleUrls: ['./widget.component.scss']
 })
 export class WidgetComponent implements OnInit {
- constructor(public dialog: MdDialog) {}
-
+  constructor(public dialog: MdDialog) {
+    this.emailScheduler = 'Email Scheduler';
+    this.serversConfigurator = 'Servers Configurator'
+  }
+  @Input() serversConfigurator: string;
+  @Input() emailScheduler: string;
   @Input() title: string;
   @Input() status: string;
   @Input() data: string;
-public  openDialog() {
-if(this.title=="Email Scheduler")
-   { this.dialog.open(EmailScheduler);}
-else if(this.title=="Servers Configurator"){
-  this.dialog.open(ServersConfigurator);
-}}
-  ngOnInit() {
+  public openDialog() {
+    if (this.title === this.emailScheduler)
+    { this.dialog.open(EmailScheduler); }
+    else if (this.title === this.serversConfigurator) {
+      this.dialog.open(ServersConfigurator);
+    }
   }
-
+  ngOnInit() { }
 }
