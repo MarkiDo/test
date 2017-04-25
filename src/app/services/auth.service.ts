144fea1f8deeab777435b7e7ce4afe0e6e4a5aad
smi1eface <smi1eface@yandex.ru>
2017-04-25 15:13:27 +0300

    fixed tslint warnings in user service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,27 +1,26 @@
-import { Injectable }      from '@angular/core';
+import { Injectable } from '@angular/core';
 import { tokenNotExpired } from 'angular2-jwt';
 
-let Auth0Lock = require('auth0-lock').default;
 @Injectable()
 export class AuthService {
-  lock = new Auth0Lock('g92ByoNQKvIcIjtJvCGps05BK6Rtw6bC', 'lv-232.eu.auth0.com', {  
+  public profile: Object;
+  private Auth0Lock = require('auth0-lock').default;
+  private lock = new this.Auth0Lock('g92ByoNQKvIcIjtJvCGps05BK6Rtw6bC', 'lv-232.eu.auth0.com', {
     closable: false,
     auth: {
       redirect: false,
     }
-});
-  profile: Object;
+  });
   constructor() {
     this.profile = JSON.parse(localStorage.getItem('profile'));
-    this.lock.on("authenticated", (authResult) => {
+    this.lock.on('authenticated', (authResult) => {
       localStorage.setItem('id_token', authResult.idToken);
       this.lock.getProfile(authResult.idToken, (error, profile) => {
         if (error) {
-          console.log("Error during getting user profile: "+ error);
-        }
-        else{
-        localStorage.setItem('profile', JSON.stringify(profile));
-        this.profile = profile;
+          console.log('Error during getting user profile: ' + error);
+        } else {
+          localStorage.setItem('profile', JSON.stringify(profile));
+          this.profile = profile;
         }
       });
       this.lock.hide();
