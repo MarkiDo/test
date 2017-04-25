e338f985e18c01dfb10375c4c336ed4786db072b
Irina Makaruk <irunamakaruk@gmail.com>
2017-04-25 13:35:28 +0000

    add functions for opening modal dialogs

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { TranslateService } from '@ngx-translate/core';
+import { EmailSchedulerDialogComponent } from './email-scheduler-dialog/email-scheduler-dialog.component';
+import { ServersConfiguratorDialogComponent } from './servers-configurator-dialog/servers-configurator-dialog.component';
 
 @Component({
   selector: 'ita-dashboard',
@@ -9,10 +11,22 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class DashboardComponent {
 
-  constructor(private translate: TranslateService, private auth: AuthService) {
+  constructor(private translate: TranslateService, private auth: AuthService, public dialog: MdDialog) {
     translate.addLangs(['en', 'ua']);
     translate.setDefaultLang('en');
     let browserLang = translate.getBrowserLang();
     translate.use(browserLang.match(/en|ua/) ? browserLang : 'en');
   }
+ private openServersConfiguratorDialog(){
+   this.dialog.open(ServersConfiguratorDialogComponent, {
+       height: '400px',
+       width: '600px',
+   });
+ }
+ private openEmailSchedulerDialog(){
+   this.dialog.open(EmailSchedulerDialogComponent, {
+       height: '400px',
+       width: '600px',
+   });
+ }
 }
