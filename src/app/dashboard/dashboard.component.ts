62b2c50fc0bdb4029aad223aef0fbd66993ddc3c
Irina Makaruk <irunamakaruk@gmail.com>
2017-04-25 15:46:17 +0000

    added translate and firebase services

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { TranslateService } from '@ngx-translate/core';
-import { EmailSchedulerDialogComponent } from './email-scheduler-dialog/email-scheduler-dialog.component';
-import { ServersConfiguratorDialogComponent } from './servers-configurator-dialog/servers-configurator-dialog.component';
+import { MdDialog } from '@angular/material';
+import {
+  EmailSchedulerDialogComponent
+} from './email-scheduler-dialog/email-scheduler-dialog.component';
+import {
+  ServersConfiguratorDialogComponent
+} from './servers-configurator-dialog/servers-configurator-dialog.component';
 
 @Component({
   selector: 'ita-dashboard',
@@ -11,22 +16,24 @@ import { ServersConfiguratorDialogComponent } from './servers-configurator-dialo
 })
 export class DashboardComponent {
 
-  constructor(private translate: TranslateService, private auth: AuthService, public dialog: MdDialog) {
+constructor(private translate: TranslateService,
+            private auth: AuthService,
+            public dialog: MdDialog) {
     translate.addLangs(['en', 'ua']);
     translate.setDefaultLang('en');
     let browserLang = translate.getBrowserLang();
     translate.use(browserLang.match(/en|ua/) ? browserLang : 'en');
   }
- private openServersConfiguratorDialog(){
-   this.dialog.open(ServersConfiguratorDialogComponent, {
-       height: '400px',
-       width: '600px',
-   });
- }
- private openEmailSchedulerDialog(){
-   this.dialog.open(EmailSchedulerDialogComponent, {
-       height: '400px',
-       width: '600px',
-   });
- }
+ private openServersConfiguratorDialog() {
+  this.dialog.open(ServersConfiguratorDialogComponent, {
+    height: '400px',
+    width: '600px'
+    });
+  }
+ private openEmailSchedulerDialog() {
+  this.dialog.open(EmailSchedulerDialogComponent, {
+    height: '400px',
+    width: '600px'
+    });
+    }
 }
--- src/app/dashboard/email-scheduler-dialog/email-scheduler-dialog.component.ts
+++ src/app/dashboard/email-scheduler-dialog/email-scheduler-dialog.component.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'app/services/auth.service';
+import { FirebaseService } from 'app/services/firebase.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'ita-email-scheduler-dialog',
   templateUrl: './email-scheduler-dialog.component.html',
   styleUrls: ['./email-scheduler-dialog.component.scss']
 })
-export class EmailSchedulerDialogComponent {}
+export class EmailSchedulerDialogComponent {
+private name: string = '';
+private description: string = '';
+private data: Object;
+private newSettings: Object;
+
+constructor(private translate: TranslateService,
+            private auth: AuthService,
+            private firebaseService: FirebaseService) {
+    translate.addLangs(['en', 'ua']);
+    translate.setDefaultLang('en');
+    let browserLang = translate.getBrowserLang();
+    translate.use(browserLang.match(/en|ua/) ? browserLang : 'en');
+    if (auth.authenticated()) {
+    firebaseService.getSettings().subscribe((data) => { this.data = data.settings; });
+    }
+  }
+  private onSubmit(form: any) {
+    this.newSettings = { form } ;
+    this.firebaseService.saveSettings(this.newSettings);
+  }
+}
