42546f3c059dbc3d7ac5406f681e7fa8729dbb63
Anton <medvedev.workmail@gmail.com>
2017-05-02 22:29:33 +0300

    fix deploy crash(fix small bugs tslint and path)

--- src/app/dashboard/email-scheduler-dialog/email-scheduler-dialog.component.ts
+++ src/app/dashboard/email-scheduler-dialog/email-scheduler-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FirebaseService } from 'app/services/firebase.service';
+import { FirebaseService } from './../../services/firebase.service';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
