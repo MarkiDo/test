6150c5d5fd55fcff2ae66bd6e1c25121243a4983
smi1eface <smi1eface@yandex.ru>
2017-05-02 21:15:56 +0300

    settings model, save results

--- src/app/dashboard/email-scheduler-dialog/email-scheduler-dialog.component.ts
+++ src/app/dashboard/email-scheduler-dialog/email-scheduler-dialog.component.ts
@@ -2,27 +2,30 @@ import { Component } from '@angular/core';
 import { AuthService } from 'app/services/auth.service';
 import { FirebaseService } from 'app/services/firebase.service';
 import { TranslateService } from '@ngx-translate/core';
-
+import { Settings } from './settings.model';
 @Component({
   selector: 'ita-email-scheduler-dialog',
   templateUrl: './email-scheduler-dialog.component.html',
   styleUrls: ['./email-scheduler-dialog.component.scss']
 })
 export class EmailSchedulerDialogComponent {
-public name: string = '';
-public data: Object;
-private description: string = '';
-private newSettings: Object;
-
-constructor(private translate: TranslateService,
-            private auth: AuthService,
-            private firebaseService: FirebaseService) {
-    if (auth.authenticated()) {
-    firebaseService.getSettings().subscribe((data) => { this.data = data.settings; });
-    }
+  public name: string = '';
+  public settings: Settings;
+  public saved: boolean;
+  public error: boolean;
+  private description: string = '';
+  private newSettings: Object;
+  constructor(private translate: TranslateService,
+              private auth: AuthService,
+              private firebaseService: FirebaseService) {
+    firebaseService.getSettings().subscribe((result) => {
+      this.settings = new Settings(result.EmailSchedulerSettings);
+    });
   }
-  public onSubmit(form: any) {
-    this.newSettings = { form } ;
-    this.firebaseService.saveSettings(this.newSettings);
+  public onSubmit(form: Settings) {
+    this.firebaseService.saveSettings(form).then((success) =>
+      this.saved = true)
+      .catch((error) =>
+        this.error = true);
   }
 }
