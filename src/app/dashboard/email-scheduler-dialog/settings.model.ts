28c288709aaa0bbf0995a88c6429bc65a71b989e
smi1eface <smi1eface@yandex.ru>
2017-05-02 21:10:39 +0300

    added settings model

---
+++ src/app/dashboard/email-scheduler-dialog/settings.model.ts
@@ -0,0 +1,12 @@
+export class Settings {
+  public name: string;
+  public description: string;
+  public option: string;
+  public date: Object;
+  constructor(obj?: any) {
+    this.name = obj && obj.name || null;
+    this.description = obj && obj.description || null;
+    this.option = obj && obj.option || null;
+    this.date = obj && obj.date || null;
+  }
+}
--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -2,27 +2,23 @@ import { Injectable } from '@angular/core';
 import { AngularFire, AngularFireModule, FirebaseObjectObservable } from 'angularfire2';
 import { NgModule } from '@angular/core';
 import { AuthService } from './auth.service';
+import { Settings } from 'app/dashboard/email-scheduler-dialog/settings.model';
 @Injectable()
 export class FirebaseService {
   public settings: FirebaseObjectObservable<any>;
   public user: any;
   public userData: FirebaseObjectObservable<any>;
   constructor(private auth: AuthService, private angularFire: AngularFire) {
+    this.user = this.auth.profile;
   }
 
   public getSettings() {
-    this.user = this.auth.profile;
     this.settings = this.angularFire.database.object('/users/' + this.user.user_id);
     return this.settings;
   }
 
-  public saveSettings(newSettings: Object) {
+  public saveSettings(newSettings: Settings) {
     this.userData = this.getSettings();
-    this.userData.set({ settings: newSettings }).then((success) => {
-      console.log('Settings saved successfully');
-    })
-      .catch((error) => {
-        console.log('Error during settings saving: ' + error);
-      });
+    return this.userData.set({ EmailSchedulerSettings: newSettings });
   }
 }
